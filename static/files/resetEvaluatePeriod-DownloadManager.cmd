5d22bdea503653cd7b6f73dc0d139d273e90867f
zhmhbest <zhmhbest@gmail.com>
2020-09-21 18:25:42 +0800

    download manager

---
+++ static/files/resetEvaluatePeriod-DownloadManager.cmd
@@ -0,0 +1,14 @@
+@ECHO OFF
+REG DELETE "HKCU\SOFTWARE\DownloadManager" /f
+IF %PROCESSOR_ARCHITECTURE%==AMD64 (
+    REG DELETE "HKLM\SOFTWARE\WOW6432Node\Internet Download Manager" /f
+    REG DELETE "HKLM\SOFTWARE\WOW6432Node\Microsoft\DownloadManager" /f
+    REM REG DELETE "HKLM\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Internet Download Manager" /f
+) ELSE (
+    ECHO Dose not support win32 system!
+    PAUSE>NUL & EXIT
+)
+
+RMDIR /s /q "%AppData%\IDM"
+ECHO Finished!
+PAUSE>NUL
