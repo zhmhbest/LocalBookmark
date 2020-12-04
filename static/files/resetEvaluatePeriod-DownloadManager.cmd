8e24818fcfcb04f34cf4b75c5d7e736b15f74ddd
zhmhbest <zhmhbest@gmail.com>
2020-12-04 18:22:49 +0800

    UPDATE

--- static/files/resetEvaluatePeriod-DownloadManager.cmd
+++ static/files/resetEvaluatePeriod-DownloadManager.cmd
@@ -1,6 +1,8 @@
 @ECHO OFF
 REG DELETE "HKCU\SOFTWARE\DownloadManager" /f
 IF %PROCESSOR_ARCHITECTURE%==AMD64 (
+    REM REG DELETE "HKCR\IDMan.CIDMLinkTransmitter" /f
+    REG DELETE "HKCU\SOFTWARE\DownloadManager" /f
     REG DELETE "HKLM\SOFTWARE\WOW6432Node\Internet Download Manager" /f
     REG DELETE "HKLM\SOFTWARE\WOW6432Node\Microsoft\DownloadManager" /f
     REM REG DELETE "HKLM\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Internet Download Manager" /f
