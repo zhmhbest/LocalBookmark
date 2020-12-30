61d18654f0d80076e5c3e97a949c473eb92d20bd
zhmhbest <zhmhbest@gmail.com>
2020-12-30 10:29:09 +0800

    自动搜索

--- data/__init__.js
+++ data/__init__.js
@@ -2,12 +2,51 @@ const path = require('path');
 const fs = require('fs');
 // const crypto = require('crypto');
 
-// 加载书签
-let bookmarks = [];
-for (let item of require('./__init__.json')) {
-    bookmarks = bookmarks.concat(require(`./${item}.json`));
+
+/**
+ * @callback ReadDirectoryCallback
+ * @param {string} name
+ * @param {fs.Stats} stat
+ */
+/**
+ * 枚举目录下所有文件和文件夹
+ * @param {string} location
+ * @param {ReadDirectoryCallback} callback
+ */
+const openDirectory = (location, callback) => {
+    try {
+        const stat = fs.statSync(location);
+        if(!stat.isDirectory()) return false;
+        callback(location, stat);
+    } catch(err) { return false }
+    const files = fs.readdirSync(location);
+    for (let file of files) {
+        const name = path.join(location, file);
+        const stat = fs.statSync(name);
+        callback(name, stat);
+        if (stat.isDirectory()) {
+            openDirectory(name, callback);
+        }
+    }
+    return true;
 }
-const fp = fs.openSync(path.join(__dirname, "../src/assets/data/bookmark.json"), 'w');
-// Buffer.from(JSON.stringify(bookmarks)).toString('base64')
+
+// 加载书签
+function loadBookmarks() {
+    let bookmarks = [];
+    openDirectory(".", (name, stat) => {
+        name = name.replace('\\', '/');
+        if (stat.isFile() && name.match(/\.json$/)) {
+            bookmarks = bookmarks.concat(require(`./${name}`));
+        }
+    });
+    return bookmarks;
+}
+
+// const __to = "../src/assets/data/bookmark.json";
+const __to = "./bookmark.json";
+const bookmarks = loadBookmarks();
+const fp = fs.openSync(path.join(__dirname, __to), 'w');
+Buffer.from(JSON.stringify(bookmarks)).toString('base64')
 fs.writeSync(fp, `${JSON.stringify(bookmarks)}`);
 fs.closeSync(fp);
