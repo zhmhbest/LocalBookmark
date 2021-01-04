a8822113065b9cd969cd09c1acf9ec53ed0f7680
zhmhbest <zhmhbest@gmail.com>
2021-01-04 16:28:03 +0800

    增强标签

--- data/__init__.js
+++ data/__init__.js
@@ -4,13 +4,33 @@ const file = require('./lib/file');
 
 // 加载书签
 function loadBookmarks() {
+    const mapTags = (() => {
+        const map = require('./manifest.json');
+        return (tags) => {
+            tags = new Set(tags.map((v, i) => {
+                const val = map[v];
+                return undefined === val ? v : val;
+            }));
+            return [...tags];
+        }
+    })();
+
     const dataPath = path.relative(".", path.join(__dirname, "bookmarks"));
     let buffer = [];
     file.openDirectory(dataPath, (name, stat) => {
         if (stat.isFile() && name.match(/\.json$/)) {
+
             const filename = `./${path.relative(__dirname, name)}`.replace(/\\/g, '/');
+            const dirs = path.dirname(filename).match(/(?<=.+bookmarks\/).+/)[0].split('/');
+            const base = path.basename(filename).match(/.+?(?=\.json)/)[0];
+            const baseTags = (dirs.concat(base)).filter(x => '_' !== x);
+
             const bookmarks = require(filename);
-            // for (let item of bookmarks) { item['ref'] = filename; }
+            for (let item of bookmarks) {
+                item.tag = mapTags(
+                    undefined === item.tag ? baseTags : item.tag.concat(baseTags)
+                ).sort();
+            }
             buffer = buffer.concat(bookmarks);
         }
     });
@@ -18,12 +38,12 @@ function loadBookmarks() {
 }
 
 
-function main(__to__) {
-    const bookmarks = loadBookmarks();
+const bookmarks = loadBookmarks();
+function saveBookmarks(__to__) {
     file.saveJSON(path.join(__dirname, __to__), bookmarks);
 }
 
 
-main("../docs/data/bookmark.json");
-main("../src/assets/data/bookmark.json");
-// main("./bookmark.json");
+saveBookmarks("../docs/data/bookmark.json");
+saveBookmarks("../src/assets/data/bookmark.json");
+saveBookmarks("./bookmark.json");
